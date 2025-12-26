c07064c7e8b7cb0fb1e5cfd83a76f12bee77ca3b
EdoFA12 <edo.broo12@gmail.com>
2025-12-26 20:16:39 +0700

    feat: add field to the default users table

--- database/migrations/2025_12_25_102810_create_human_resources_app.php
+++ database/migrations/2025_12_25_102810_create_human_resources_app.php
@@ -11,6 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
+       
+
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('fullname');
@@ -19,75 +38,58 @@ return new class extends Migration
             $table->string('address');
             $table->date('birth_date');
             $table->date('hire_date');
-            $table->foreignKey('department_id')->constrained('departments');
-            $table->foreignKey('role_id')->constrained('roles');
+            $table->foreignId('department_id')->constrained('departments');
+            $table->foreignId('role_id')->constrained('roles');
             $table->string('status');
             $table->decimal('salary', 10, 2);
             $table->timestamps();
-            $table->softDelates();
+            $table->softDeletes();
 
-        });
-
-        Schema::create('departments', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->string('status');
-            $table->timestamps();
-            $table->softDelates();
-        });
-
-        Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('description')->nullable();
-            $table->timestamps();
-            $table->softDelates();
         });
 
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('description')->nullable();
-            $table->foreignKey('assigned_to')->constrained('employees');
+            $table->foreignId('assigned_to')->constrained('employees');
             $table->date('due_date');
             $table->string('status');
             $table->timestamps();
-            $table->softDelates();
+            $table->softDeletes();
         });
 
         Schema::create('payroll', function (Blueprint $table) {
             $table->id();
-            $table->foreignKey('employee_id')->constrained('employees');
+            $table->foreignId('employee_id')->constrained('employees');
             $table->decimal('salary', 10, 2);
             $table->decimal('bonuses', 10, 2)->nullable();
             $table->decimal('deductions', 10, 2)->nullable();
             $table->decimal('net_salary', 10, 2);
             $table->date('pay_date');
             $table->timestamps();
-            $table->softDelates();
+            $table->softDeletes();
         });
 
         Schema::create('presences', function (Blueprint $table) {
             $table->id();
-            $table->foreignKey('employee_id')->constrained('employees');
+            $table->foreignId('employee_id')->constrained('employees');
             $table->date('check_in');
             $table->date('check_out');
             $table->date('date');
             $table->string('status');
             $table->timestamps();
-            $table->softDelates();
+            $table->softDeletes();
         });
 
         Schema::create('leave_requests', function (Blueprint $table) {
             $table->id();
-            $table->foreignKey('employee_id')->constrained('employees');
+            $table->foreignId('employee_id')->constrained('employees');
             $table->string('leave_type');
             $table->date('start_date');
             $table->date('end_date');
             $table->string('status');
             $table->timestamps();
-            $table->softDelates();
+            $table->softDeletes();
          });
 
     }
